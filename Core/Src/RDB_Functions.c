be6ecf30fede5624f0ad479c106e4d17cb363025
sam neumann <sam.neumann2@gmail.com>
2025-11-07 19:06:44 -0800

    Started Creating test function for display layout

--- Core/Src/RDB_Functions.c
+++ Core/Src/RDB_Functions.c
@@ -6,3 +6,13 @@
  */
 
 
+#include "ST7565.h"
+#include"RDB_depth_digits_36x58.h"
+
+const unsigned char *depthDigitArray[10]= {BM0_36x58_block, BM1_36x58_block, BM2_36x58_block, BM3_36x58_block, BM4_36x58_block, BM5_36x58_block, BM6_36x58_block,BM7_36x58_block,BM8_36x58_block,BM9_36x58_block};
+
+void countTestRDBlcd(){
+
+
+
+}
